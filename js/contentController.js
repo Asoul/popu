b50e4a2909a613035d774c84c255d593fa9864f5
Asoul <b00902036@ntu.edu.tw>
2015-03-16 21:44:36 +0800

    fix

--- js/contentController.js
+++ js/contentController.js
@@ -1,5 +1,5 @@
 var app = angular.module('myApp', []);
-app.controller('customersController', ['$scope', '$http', function($scope,$http) {
+app.controller('contentController', ['$scope', '$http', function($scope,$http) {
   $http.get('https://graph.facebook.com/v2.2/136845026417486/feed')
   .success(function(response){
     $scope.feed_data = response.data;
